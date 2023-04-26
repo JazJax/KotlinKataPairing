edb01918aa524b7199d1bb8a3e84edd7ae15b133
pedrosoares <pedro.soares@madetech.com>
2023-04-26 16:36:46 +0100

    solution for AlphabetWar

--- KotlinKataPairing/src/main/kotlin/AlphabetWar.kt
+++ KotlinKataPairing/src/main/kotlin/AlphabetWar.kt
@@ -1,5 +1,30 @@
+//left side
+val leftSide = mapOf(
+    'w' to 4,
+    'p' to 3,
+    'b' to 2,
+    's' to 1
+)
+
+//right side
+val rightSide = mapOf(
+    'm' to 4,
+    'q' to 3,
+    'd' to 2,
+    'z' to 1
+)
 
 fun alphabetWar(fight: String): String {
-   return ""
+    val splitList = fight.partition { it in rightSide }
+    val rightSideSum = splitList.first.sumOf { rightSide[it]?: 0 }
+    val leftSideSum = splitList.second.sumOf { leftSide[it]?: 0 }
+
+    val total = rightSideSum - leftSideSum
+   return when {
+        total == 0 -> "Let's fight again!"
+        total > 0 -> "Right side wins!"
+        total < 0 -> "Left side wins!"
+       else -> "Let's fight again!"
+   }
 }
 
--- KotlinKataPairing/src/test/kotlin/AlphabetWarKtTest.kt
+++ KotlinKataPairing/src/test/kotlin/AlphabetWarKtTest.kt
@@ -3,9 +3,30 @@ import org.junit.jupiter.api.Test
 
 class AlphabetWarKtTest{
     @Test
-    fun `Should Right side wins! when right side stronger than left side`() {
-        val expected =""
+    fun `Should return Right side wins! when right side stronger than left side`() {
+        val expected =alphabetWar("zzzzsm")
         assertEquals("Right side wins!", expected)
     }
 
+    @Test
+    fun `Should return Left side wins! when right side weaker than left side`() {
+        val expected =alphabetWar("wwwwwwmm")
+        assertEquals("Left side wins!", expected)
+    }
+
+    @Test
+    fun `Should return Let's fight again! when right side strength is equals to left side`() {
+        val expected =alphabetWar("wmqpqpqpwwmm")
+        assertEquals("Let's fight again!", expected)
+    }
+    @Test
+    fun `Should return Let's fight again! when string is empty`() {
+        val expected =alphabetWar("")
+        assertEquals("Let's fight again!", expected)
+    }
+    @Test
+    fun `Should give value 0 to values that are not left or right`() {
+        val expected =alphabetWar("a")
+        assertEquals("Let's fight again!", expected)
+    }
 }
