7af6a80ade3b7ccd5b3c6ec89ddc601ff8ceb0b1
mandanakhademi <mandanakhademi88@gmail.com>
2023-04-26 15:41:14 +0100

    Add link to the kata

--- KotlinKataPairing/src/main/kotlin/Office1.kt
+++ KotlinKataPairing/src/main/kotlin/Office1.kt
@@ -1,4 +1,6 @@
-
+/*
+Here is link to the kata: https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/kotlin
+ */
 fun outed(meet: Map<String, Int>, boss: String): String {
     if(meet.any { it.value > 10 }){return "Choose a valid score!"}
     if ((meet.values.sum() + meet[boss]!!) / meet.size >= 5) return "Nice Work Champ!"
