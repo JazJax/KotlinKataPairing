d53c861958f0e929f9aafd51c544010a7d1e4528
mandanakhademi <mandanakhademi88@gmail.com>
2023-04-26 11:19:06 +0100

    Update Office function

--- KotlinKataPairing/src/main/kotlin/Office1.kt
+++ KotlinKataPairing/src/main/kotlin/Office1.kt
@@ -1,4 +1,23 @@
+
 fun outed(meet: Map<String, Int>, boss: String): String {
-return ""
+    var average:Int = 0
+    var total:Int = 0
+    for (member in meet.keys) {
+        if (member != boss) {
+            total += meet[member]!!
+        } else {
+            total = total + meet[member]!! * 2
+        }
+
+    }
+    average = total / meet.size
+    if (average >= 5) {
+        return "Nice Work Champ!"
+    }
+    return "Get Out Now!"
+//    if (meet[boss]!! >= 5) {
+//        return "Nice Work Champ!"
+//    }
+//    return "Get Out Now!"
 }
 
--- KotlinKataPairing/src/test/kotlin/Office1Test.kt
+++ KotlinKataPairing/src/test/kotlin/Office1Test.kt
@@ -1,13 +1,49 @@
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.testng.annotations.Test
 
 internal class Office1Test {
-    //    private val testSample: Offi = Sample()
+//    private val testSample: Office1Test = Office1Test()
     //    fun outed(meet: Map<String, Int>, boss: String): String {
     @Test
-    fun `outed should return Get Out Now! when happiness bellow 5`() {
+    fun `outed should return Get Out Now! when happiness is empty`() {
         val meet = mapOf("" to 0)
         val result = outed(meet, "")
         assertEquals("Get Out Now!",result)
     }
+
+    @Test
+    fun `outed should return Nice Work Champ! when happiness is 5 or greater than 5`() {
+        val meet = mapOf("" to 5)
+        val result = outed(meet, "")
+        assertEquals("Nice Work Champ!",result)
+    }
+
+    @Test
+    fun `get list of people outed should return Nice Work Champ! when happiness is 5 or greater than 5`() {
+        val meet = mapOf("Jasper" to 5, "Danny" to 6)
+        val result = outed(meet, "Jasper")
+        assertEquals("Nice Work Champ!",result)
+    }
+
+    @Test
+    fun `get list of people outed should return Nice Work Champ! when happiness is 5 and boss happiness is below 5`() {
+        val meet = mapOf("Jasper" to 6, "Danny" to 6, "Pedro" to 4)
+        val result = outed(meet, "Pedro")
+        assertEquals("Nice Work Champ!",result)
+    }
+
+    @Test
+    fun `get list of people outed should return Get Out Now! when happiness is below 5 and boss happiness is below 5`() {
+        val meet = mapOf("Jasper" to 2, "Danny" to 4, "Pedro" to 4)
+        val result = outed(meet, "Pedro")
+        assertEquals("Get Out Now!",result)
+    }
+
+    @Test
+    fun `get list of people outed should return Get Out Now! when happiness is below 5 and boss happiness is greater than 5`() {
+        val meet = mapOf("Jasper" to 1, "Danny" to 2, "Pedro" to 5)
+        val result = outed(meet, "Pedro")
+        assertEquals("Get Out Now!",result)
+    }
+
 }
