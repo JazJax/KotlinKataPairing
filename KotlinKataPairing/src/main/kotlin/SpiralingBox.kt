f13da914fc833a79ac5719324c9cf0816b9d3372
mandanakhademi <mandanakhademi88@gmail.com>
2023-04-28 12:12:58 +0100

    Update Spiraling box exercise

--- KotlinKataPairing/src/main/kotlin/SpiralingBox.kt
+++ KotlinKataPairing/src/main/kotlin/SpiralingBox.kt
@@ -1,4 +1,30 @@
+/*
+Here is link to the kata: https://www.codewars.com/kata/63b84f54693cb10065687ae5/kotlin
+ */
 fun createBox(width: Int, length: Int): Array<IntArray> {
-    intArrayOf(width, length) = (1,1)
-    return Array(length) { IntArray(width) }
+    val arr = Array(length) { IntArray(width) }
+
+    var leftCol = 0 // it equals topRow
+    var rightCol = width - 1
+    var downRow = length - 1
+    var element = 1
+
+    while (leftCol <= rightCol && leftCol <= downRow) {
+        for (i in leftCol..rightCol) {
+            arr[leftCol][i] = element
+            arr[downRow][i] = element
+        }
+
+        for (j in leftCol..downRow){
+            arr[j][leftCol] = element
+            arr[j][rightCol] =  element
+        }
+
+        leftCol += 1
+        rightCol -= 1
+        downRow -= 1
+        element += 1
+    }
+
+    return arr
 }
