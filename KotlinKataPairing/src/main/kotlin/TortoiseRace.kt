5a82166fd7b435ba7675d2c8276999ada18f18ef
dtzz81 <denisa.turkova@madetech.com>
2023-05-06 11:36:30 +0100

    Tortoise Race exercise,refactor, 1 failing, code not yet finished

--- KotlinKataPairing/src/main/kotlin/TortoiseRace.kt
+++ KotlinKataPairing/src/main/kotlin/TortoiseRace.kt
@@ -2,45 +2,21 @@
 
 package tortoise
 
-import kotlin.concurrent.timer
 
-// return [] if v1 >= v2
 fun race(v1:Int, v2:Int, g:Int):IntArray {
-    // in 30 mins: v1 360 + 70, v2 425
-    // in 1 hour: v1 720 + 70, v2 850
-    // n = time in mins
 
-    // starting 0 time
-    // they did not catch up = false until they catch up
-
-    val start_time: Int = 0
-    var time: Int = start_time + 1
-    var catchup:Boolean = false
-    while (catchup:Boolean is false) {
-
-        var DistTort1 :Int = v1 * time + g
-        var DistTort2 :Int = v2 * time
-        var distance :Int = DistTort1 - DistTort2
-        if (distance > 0) {
-            catchup is false
-
-    }   else {
-            catchup is true
+    var time = 0
+    var catchup = false
+    while (!catchup)  {
+        time++
+        var distTort1 :Int = v1 * time + g
+        var distTort2 :Int = v2 * time
+        var distance :Int = distTort2 - distTort1
+        if (distance >= 0) {
+            catchup = true
         }
+    }
 
 
-   // var time = 0
-    //var diffFlag:Boolean = false
-    //while  (diffFlag is false) {
-      //      var distance1 :Int = (v1 * time) + g
-        //    var distance2 :Int = v2 * time
-          //  var difference :Int = distance1 - distance2
-            //if "difference" is 0:
-            //diffFlag == true
-            //var time = time + 1
-            //break
-    //} }
-
-
-    return intArrayOf()
+    return intArrayOf(time, v1 * time + g, v2 * time)
 }
